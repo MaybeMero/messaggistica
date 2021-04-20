e9f919f4c6297e9ede0e3faa27fe2e75c33414ba
MaybeMero <mero.goyes.maybe.yuleysy@iisgalvanimi.edu.it>
2021-04-20 07:54:59 +0000

    punto 1,2 e 3

--- client-socketio/src/app/cesar.service.ts
+++ client-socketio/src/app/cesar.service.ts
@@ -3,18 +3,19 @@ import { Injectable } from '@angular/core';
 export class CesarService {
     decode(encoded:string, offset:number) : string
     {
-        //Trasformo la stringa in un array di carateri
+      //Trasformo la stringa in un array di carateri
         let charArray = Array.from(encoded);
         let decodedArray = [];
         for (const char of charArray) {
             //Estraggo da ogni carattere il codice ascii
             let charCode = char.charCodeAt(0);
             //Modifico il codice ascii e lo salvo in un carattere
-            let newChar = String.fromCharCode(charCode - offset);
+            let newChar = String.fromCharCode(charCode - offset)
             decodedArray.push(newChar);
         }
         //Trasformo l'encoded array in una stringa
         return decodedArray.join('');
+
     }
 
     encode(toEncode:string, offset:number) : string
